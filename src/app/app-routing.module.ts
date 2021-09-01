e952cd152d5fd4f357fd66319748e400fe6c8a84
Billy Rodriguez <billyvecuss@hotmail.com>
2021-09-01 11:48:53 -0500

    creating and configurating routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, Router } from "@angular/router";
+import { AboutComponent } from "./pages/about/about.component";
+import { ItemComponent } from "./pages/item/item.component";
+import { PortfolioComponent } from "./pages/portfolio/portfolio.component";
+const app_routes: Routes = [
+    { path: "home", component: PortfolioComponent },
+    { path: "about", component: AboutComponent },
+    { path: "item", component: ItemComponent },
+    { path: "**", pathMatch: "full", redirectTo: "home" }
+]
+// Take app_modules and set to RouterModule decorator
+@NgModule({
+    imports: [
+        RouterModule.forRoot(app_routes, { useHash: true })
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
